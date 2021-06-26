0b1c542e1645afe5fc9a16868c6e9257bd70aff3
junfuture1103 <junhak1103@naver.com>
2021-06-26 22:45:55 +0900

    1st complete but..

--- tcp_block/main.cpp
+++ tcp_block/main.cpp
@@ -6,6 +6,7 @@
 
 #define TRUE 1
 #define FALSE 0
+#define MESSAGE_SIZE 10
 
 #pragma pack(push, 1)
 
@@ -14,10 +15,16 @@ struct EthArpPacket final {
     ArpHdr arp_;
 };
 
+struct tcp_data{
+    char msg[MESSAGE_SIZE];
+    int msg_size = MESSAGE_SIZE;
+};
+
 struct EthPacket{
     EthHdr eth_;
     libnet_ipv4_hdr ip_hdr_v4;
     libnet_tcp_hdr tcp_hdr;
+    tcp_data data;
 };
 
 struct Pseudoheader{
@@ -188,7 +195,7 @@ void SendPacket(pcap_t* handle, const u_char* packet, int packet_size){
     if (res != 0) {
         fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
     }else{
-        printf("Sending forward_rst block packet..\n");
+        printf("Sending block packet..\n");
     }
 }
 
@@ -223,15 +230,149 @@ void forward_rst(Mac MAC_ADD, pcap_t* handle, const u_char* buf){
     packet->tcp_hdr.th_seq = tcp_hdr->th_seq + tcp_data_size;
     packet->tcp_hdr.th_ack = tcp_hdr->th_ack;
     packet->tcp_hdr.th_off = sizeof(libnet_tcp_hdr)/4;
-    packet->tcp_hdr.th_flags = TH_RST | TH_ACK; //Fin : tcp_hdr_pk->th_flags = TH_FIN | TH_ACK | TH_PSH;
-    packet->tcp_hdr.th_sum = calTCPChecksum((u_int8_t*)ip_hdr_v4, ntohs(ip_hdr_v4->ip_len));
+    packet->tcp_hdr.th_flags = TH_RST | TH_ACK; //Rst block. Fin : tcp_hdr_pk->th_flags = TH_FIN | TH_ACK | TH_PSH;
+    packet->tcp_hdr.th_sum = calTCPChecksum((u_int8_t*)ip_hdr_v4, ntohs(packet->ip_hdr_v4.ip_len));
 
     printf("================== made packet ==================\n");
-    packet_size = sizeof(libnet_ethernet_hdr) + sizeof(libnet_ipv4_hdr) + sizeof(libnet_tcp_hdr);
+    packet_size = sizeof(libnet_ethernet_hdr) + htons(packet->ip_hdr_v4.ip_len);
     dump((u_char*)packet, packet_size);
 
     SendPacket(handle, (const u_char*)packet, packet_size);
+}
 
+
+void backward_rst(Mac MAC_ADD, pcap_t* handle, const u_char* buf){
+    libnet_ethernet_hdr *eth_hdr = (libnet_ethernet_hdr*)buf;
+    libnet_ipv4_hdr *ip_hdr_v4 = (libnet_ipv4_hdr*)(buf + sizeof(libnet_ethernet_hdr));
+    libnet_tcp_hdr *tcp_hdr = (libnet_tcp_hdr*)(buf + sizeof(libnet_ethernet_hdr) + (ip_hdr_v4->ip_hl*4));
+
+    int packet_size = sizeof(libnet_ethernet_hdr) + ntohs(ip_hdr_v4->ip_len);
+    int tcp_data_size = ntohs(ip_hdr_v4->ip_len) - ip_hdr_v4->ip_hl*4 - tcp_hdr->th_off*4;
+
+    printf("================== origin packet ==================\n");
+    dump((u_char*)buf, packet_size);
+
+    EthPacket* packet = (EthPacket*)buf;
+
+    //set ether header
+    packet->eth_.dmac_ = Mac(eth_hdr->ether_shost);//d_mac is org-packet
+    packet->eth_.smac_ = Mac(MAC_ADD);
+    packet->eth_.type_ = eth_hdr->ether_type;
+
+    //set ip header
+    packet->ip_hdr_v4.ip_len = ntohs(sizeof(libnet_ipv4_hdr) + sizeof(libnet_tcp_hdr)); //Here is RST block total length
+    packet->ip_hdr_v4.ip_dst = ip_hdr_v4->ip_src; //d_ip is org-packet reverse
+    packet->ip_hdr_v4.ip_src = ip_hdr_v4->ip_dst; //s_ip is org-packet reverse
+    packet->ip_hdr_v4.ip_ttl = 128; //ttl is about 128
+    packet->ip_hdr_v4.ip_sum = calIPChecksum((u_int8_t*)ip_hdr_v4);
+
+    //set tcp header
+    packet->tcp_hdr.th_dport = tcp_hdr->th_sport;//sport, dport is org-packet
+    packet->tcp_hdr.th_sport = tcp_hdr->th_dport;//sport, dport is org-packet
+    packet->tcp_hdr.th_seq = tcp_hdr->th_ack;
+    packet->tcp_hdr.th_ack = tcp_hdr->th_seq + tcp_data_size;
+    packet->tcp_hdr.th_off = sizeof(libnet_tcp_hdr)/4;
+    packet->tcp_hdr.th_flags = TH_RST | TH_ACK; //Rst block. Fin : tcp_hdr_pk->th_flags = TH_FIN | TH_ACK | TH_PSH;
+    packet->tcp_hdr.th_sum = calTCPChecksum((u_int8_t*)ip_hdr_v4, ntohs(packet->ip_hdr_v4.ip_len));
+
+    printf("================== made packet ==================\n");
+    dump((u_char*)packet, packet_size);
+
+    SendPacket(handle, (const u_char*)packet, packet_size);
+}
+
+void forward_fin(Mac MAC_ADD, pcap_t* handle, const u_char* buf){
+    libnet_ethernet_hdr *eth_hdr = (libnet_ethernet_hdr*)buf;
+    libnet_ipv4_hdr *ip_hdr_v4 = (libnet_ipv4_hdr*)(buf + sizeof(libnet_ethernet_hdr));
+    libnet_tcp_hdr *tcp_hdr = (libnet_tcp_hdr*)(buf + sizeof(libnet_ethernet_hdr) + (ip_hdr_v4->ip_hl*4));
+
+    int packet_size = sizeof(libnet_ethernet_hdr) + ntohs(ip_hdr_v4->ip_len);
+
+    std::string message = "blocked";
+
+    printf("================== origin packet ==================\n");
+    dump((u_char*)buf, packet_size);
+
+    EthPacket* packet = (EthPacket*)buf;
+
+    //set ether header
+    packet->eth_.dmac_ = Mac(eth_hdr->ether_dhost);//d_mac is org-packet
+    packet->eth_.smac_ = Mac(MAC_ADD);
+    packet->eth_.type_ = eth_hdr->ether_type;
+
+    //set ip header
+    packet->ip_hdr_v4.ip_len = ntohs(sizeof(libnet_ipv4_hdr) + sizeof(libnet_tcp_hdr) + message.size()); //Here is RST block total length
+    packet->ip_hdr_v4.ip_dst = ip_hdr_v4->ip_dst; //d_ip is org-packet
+    packet->ip_hdr_v4.ip_src = ip_hdr_v4->ip_src; //s_ip is org-packet
+    packet->ip_hdr_v4.ip_ttl = ip_hdr_v4->ip_ttl; //ttl is org-packet
+    packet->ip_hdr_v4.ip_sum = calIPChecksum((u_int8_t*)ip_hdr_v4);
+
+    //set tcp header
+    packet->tcp_hdr.th_dport = tcp_hdr->th_dport;//sport, dport is org-packet
+    packet->tcp_hdr.th_sport = tcp_hdr->th_sport;//sport, dport is org-packet
+    packet->tcp_hdr.th_seq = tcp_hdr->th_seq + message.size();
+    packet->tcp_hdr.th_ack = tcp_hdr->th_ack;
+    packet->tcp_hdr.th_off = sizeof(libnet_tcp_hdr)/4;
+    packet->tcp_hdr.th_flags = TH_FIN | TH_ACK | TH_PUSH; //Fin block
+    packet->tcp_hdr.th_sum = calTCPChecksum((u_int8_t*)ip_hdr_v4, ntohs(packet->ip_hdr_v4.ip_len));
+
+    //set tcp data
+    memcpy(packet->data.msg, message.c_str(), message.size());
+    packet->data.msg_size = message.size();
+
+    printf("================== made packet ==================\n");
+    packet_size = sizeof(libnet_ethernet_hdr) + htons(packet->ip_hdr_v4.ip_len);
+    dump((u_char*)packet, packet_size);
+
+    SendPacket(handle, (const u_char*)packet, packet_size);
+}
+
+void backward_fin(Mac MAC_ADD, pcap_t* handle, const u_char* buf){
+    libnet_ethernet_hdr *eth_hdr = (libnet_ethernet_hdr*)buf;
+    libnet_ipv4_hdr *ip_hdr_v4 = (libnet_ipv4_hdr*)(buf + sizeof(libnet_ethernet_hdr));
+    libnet_tcp_hdr *tcp_hdr = (libnet_tcp_hdr*)(buf + sizeof(libnet_ethernet_hdr) + (ip_hdr_v4->ip_hl*4));
+
+    int packet_size = sizeof(libnet_ethernet_hdr) + ntohs(ip_hdr_v4->ip_len);
+
+    printf("================== origin packet ==================\n");
+    dump((u_char*)buf, packet_size);
+
+    EthPacket* packet = (EthPacket*)buf;
+
+    std::string message = "blocked";
+
+    //set ether header
+    packet->eth_.dmac_ = Mac(eth_hdr->ether_shost);//d_mac is org-packet
+    packet->eth_.smac_ = Mac(MAC_ADD);
+    packet->eth_.type_ = eth_hdr->ether_type;
+
+    //set ip header
+    packet->ip_hdr_v4.ip_len = ntohs(sizeof(libnet_ipv4_hdr) + sizeof(libnet_tcp_hdr) + message.size()); //Here is RST block total length
+    packet->ip_hdr_v4.ip_dst = ip_hdr_v4->ip_src; //d_ip is org-packet reverse
+    packet->ip_hdr_v4.ip_src = ip_hdr_v4->ip_dst; //s_ip is org-packet reverse
+    packet->ip_hdr_v4.ip_ttl = 128; //ttl is about 128
+    packet->ip_hdr_v4.ip_sum = calIPChecksum((u_int8_t*)ip_hdr_v4);
+
+    int tcp_data_size = packet->ip_hdr_v4.ip_len - ip_hdr_v4->ip_hl*4 - tcp_hdr->th_off*4;
+
+    //set tcp header
+    packet->tcp_hdr.th_dport = tcp_hdr->th_sport;//sport, dport is org-packet
+    packet->tcp_hdr.th_sport = tcp_hdr->th_dport;//sport, dport is org-packet
+    packet->tcp_hdr.th_seq = tcp_hdr->th_ack;
+    packet->tcp_hdr.th_ack = tcp_hdr->th_seq + tcp_data_size;
+    packet->tcp_hdr.th_off = sizeof(libnet_tcp_hdr)/4;
+    packet->tcp_hdr.th_flags = TH_FIN | TH_ACK | TH_PUSH; //Fin block
+    packet->tcp_hdr.th_sum = calTCPChecksum((u_int8_t*)ip_hdr_v4, ntohs(packet->ip_hdr_v4.ip_len));
+
+    //set tcp data
+    memcpy(packet->data.msg, message.c_str(), message.size());
+
+    packet->data.msg_size = message.size();
+    printf("================== made packet ==================\n");
+    packet_size = sizeof(libnet_ethernet_hdr) + htons(packet->ip_hdr_v4.ip_len);
+    dump((u_char*)packet, packet_size);
+
+    SendPacket(handle, (const u_char*)packet, packet_size);
 }
 
 //find warning site
@@ -327,9 +468,12 @@ int main(int argc, char* argv[]) {
             if(GetInterfaceMacAddress(param.dev_, &MAC_ADD, &IP_ADD) != 0){
                 printf("have problem in GetMAC..\n");
             }
-
             //send block packet
-            forward_rst(MAC_ADD, pcap, packet);
+            //forward_rst(MAC_ADD, pcap, packet);
+            //backward_rst(MAC_ADD, pcap, packet);
+            forward_fin(MAC_ADD, pcap, packet);
+            //backward_fin(MAC_ADD, pcap, packet);
+
         }
 
         index++;
